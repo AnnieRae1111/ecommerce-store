799ecf7a75660fb287f1ae7269c747badf54a7bc
AnnieRae1111 <annierae.elizabeth@gmail.com>
2022-07-26 17:47:04 -0600

    direcoty page built out

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import './categories.styles.scss';
+import Directory from './components/directory/directory.component';
 
 function App() {
   const categories = [
@@ -30,23 +31,7 @@ function App() {
     },
   ];
 
-  return (
-    <div className="categories-container">
-      {categories.map(({ title, id, imageUrl }) => (
-        <div className="category-container">
-          <div
-            className="background-image"
-            style={{ backgroundImage: `url(${imageUrl})` }}
-          />
-          {/* <img /> */}
-          <div className="category-body-container">
-            <h2>{title}</h2>
-            <p>Shop Now</p>
-          </div>
-        </div>
-      ))}
-    </div>
-  );
+  return <Directory categories={categories} />;
 }
 
 export default App;
