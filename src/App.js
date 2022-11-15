779c44f16858c09ec5fe1efec1a2cc71f2765f1e
AnnieRae1111 <annierae.elizabeth@gmail.com>
2022-11-14 19:32:31 -0700

    set up firebase

--- src/App.js
+++ src/App.js
@@ -1,37 +1,25 @@
 import './App.css';
 import './categories.styles.scss';
-import Directory from './components/directory/directory.component';
+import Home from './components/routes/home/home';
+import { Routes, Route } from 'react-router-dom';
+import Navigation from './components/routes/navigation/navigation';
+import SignIn from './components/routes/sign-in/sign-in-component';
+
+const Shop = () => {
+  return <h1> I am the shop page</h1>;
+};
 
 function App() {
-  const categories = [
-    {
-      id: 1,
-      title: 'hats',
-      imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-    },
-    {
-      id: 2,
-      title: 'jackets',
-      imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
-    },
-    {
-      id: 3,
-      title: 'sneakers',
-      imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
-    },
-    {
-      id: 4,
-      title: 'womens',
-      imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
-    },
-    {
-      id: 5,
-      title: 'mens',
-      imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
-    },
-  ];
-
-  return <Directory categories={categories} />;
+  return (
+    <Routes>
+      <Route path="/" element={<Navigation />}>
+        <Route index={true} path="/" element={<Home />} />
+        {/* adding index to the home path makes the outlet render that automatically on the home page */}
+        <Route path="shop" element={<Shop />} />
+        <Route path="sign-in" element={<SignIn />} />
+      </Route>
+    </Routes>
+  );
 }
 
 export default App;
---
+++ src/utils/firebase/firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from 'firebase/app';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyBUZGUBOwVUaIG1vlWGen0EQT8E1F3XOrM',
+  authDomain: 'clothing-store-db-34bf8.firebaseapp.com',
+  projectId: 'clothing-store-db-34bf8',
+  storageBucket: 'clothing-store-db-34bf8.appspot.com',
+  messagingSenderId: '594491485063',
+  appId: '1:594491485063:web:9b7067a816c63acbcd0122',
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
